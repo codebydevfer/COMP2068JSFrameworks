adb2be0c8dda8f45b1b6bad9e4d7ab1d5060a4e4
Fernando <fernandopeandrade@outlook.com>
2025-12-02 12:41:47 -0500

    render and db compatibility

--- ASSIGNMENT02/pokedex-manager/config/database.js
+++ ASSIGNMENT02/pokedex-manager/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
-    }
-    catch (err) {
-        console.log("Failed to connect to MongoDB. Error: ", err)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB. Error:", err);
+  }
 }
 
 module.exports = connect;
