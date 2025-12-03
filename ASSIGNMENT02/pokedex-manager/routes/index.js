bdcce4605a19c8f410e19a25d7fe74e1e237df28
Fernando <fernandopeandrade@outlook.com>
2025-12-03 15:03:24 -0500

    fixing the edit not saving bug

--- ASSIGNMENT02/pokedex-manager/routes/index.js
+++ ASSIGNMENT02/pokedex-manager/routes/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const axios = require('axios');
+const axios = require("axios");
 const Pokemon = require("../models/pokemon");
 
 /* GET home page. (Search) */
-router.get('/', function(req, res) {
-  res.render('home', { title: 'Home Page' });
+router.get("/", function (req, res) {
+  res.render("home", { title: "Home Page" });
 });
 
 // Search Pokemon
-router.get('/search', async function(req, res) {
+router.get("/search", async function (req, res) {
   const name = req.query.name?.toLowerCase();
 
   console.log("Searching for:", name);
@@ -20,22 +20,25 @@ router.get('/search', async function(req, res) {
   }
 
   try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${name}`
+    );
 
     const pokemon = {
       id: response.data.id,
       name: response.data.name,
       image: response.data.sprites.front_default,
-      types: response.data.types.map(t => t.type.name),
-      hp: response.data.stats.find(s => s.stat.name === "hp").base_stat,
-      attack: response.data.stats.find(s => s.stat.name === "attack").base_stat,
-      defense: response.data.stats.find(s => s.stat.name === "defense").base_stat,
+      types: response.data.types.map((t) => t.type.name),
+      hp: response.data.stats.find((s) => s.stat.name === "hp").base_stat,
+      attack: response.data.stats.find((s) => s.stat.name === "attack")
+        .base_stat,
+      defense: response.data.stats.find((s) => s.stat.name === "defense")
+        .base_stat,
       height: response.data.height / 10, //converted to meters
-      weight: response.data.weight / 10 //converted to kg
+      weight: response.data.weight / 10, //converted to kg
     };
 
     res.render("searchResult", { pokemon });
-
   } catch (err) {
     console.log(err.message);
     res.render("index", { error: "Pokemon not found..." });
@@ -44,11 +47,11 @@ router.get('/search', async function(req, res) {
 
 // ADD Pokemon
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
 }
 
-router.post('/add', isLoggedIn, async (req, res) => {
+router.post("/add", isLoggedIn, async (req, res) => {
   try {
     await Pokemon.create({
       id: req.body.id,
@@ -60,43 +63,71 @@ router.post('/add', isLoggedIn, async (req, res) => {
       defense: req.body.defense,
       height: req.body.height,
       weight: req.body.weight,
-      user: req.user._id
+      user: req.user._id,
     });
-    res.redirect('/pokedex');
+    res.redirect("/pokedex");
   } catch (err) {
     res.send("Error adding your Pokemon...");
   }
 });
 
 // View all Pokemon
-router.get('/pokedex', isLoggedIn, async (req, res) => {
+router.get("/pokedex", isLoggedIn, async (req, res) => {
   const pokedex = await Pokemon.find({ user: req.user._id });
   res.render("pokedex", { pokedex });
 });
 
 // Delete Pokemon
-router.post('/delete/:id', async (req, res) => {
+router.post("/delete/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   await Pokemon.deleteOne({ id: id });
-  res.redirect('/pokedex');
+  res.redirect("/pokedex");
 });
 
 // Edit form
-router.get('/edit/:id', async (req, res) => {
-  const pokemon = await Pokemon.findOne({ id: req.params.id });
+// router.get('/edit/:id', async (req, res) => {
+//   const pokemon = await Pokemon.findOne({ id: req.params.id });
+//   res.render("edit", { pokemon });
+// });
+
+// // Edit submission
+// router.post('/edit/:id', async (req, res) => {
+//   await Pokemon.updateOne(
+//     { id: req.params.id },
+//     {
+//       name: req.body.name,
+//       image: req.body.image
+//     }
+//   );
+//   res.redirect('/pokedex');
+// });
+
+// new edit form after adding authentication
+
+router.get("/edit/:id", isLoggedIn, async (req, res) => {
+  const pokemon = await Pokemon.findOne({
+    id: req.params.id,
+    user: req.user._id,
+  });
+
+  if (!pokemon) return res.redirect("/pokedex");
+
   res.render("edit", { pokemon });
 });
 
-// Edit submission
-router.post('/edit/:id', async (req, res) => {
+router.post("/edit/:id", isLoggedIn, async (req, res) => {
   await Pokemon.updateOne(
-    { id: req.params.id },
+    {
+      id: req.params.id,
+      user: req.user._id,
+    },
     {
       name: req.body.name,
-      image: req.body.image
+      image: req.body.image,
     }
   );
-  res.redirect('/pokedex');
+
+  res.redirect("/pokedex");
 });
 
 module.exports = router;
